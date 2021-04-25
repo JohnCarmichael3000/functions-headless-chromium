9d8195421d571f519c0396f9fcfebcfcd00c9b8b
John Carmichael <johncarmichael@hotmail.com>
2021-04-24 19:48:40 -0700

    update

--- playwright/getWebSiteFullHtml/index.js
+++ playwright/getWebSiteFullHtml/index.js
@@ -11,15 +11,6 @@ module.exports = async function (context, req) {
     const urlhtml = req.query.urlhtml;
     const timewait = Number(req.query.timewait) * 1000;
 
-    //const name = (req.query.name || (req.body && req.body.name));
-    //const responseMessage = name
-      //  ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-      //  : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    //context.res = {
-        // status: 200, /* Defaults to 200 */
-      //  body: responseMessage
-    //};
-
     context.log('Function right before call to webpage: ' + urlhtml);
     const page = await browserContext.newPage();
     await page.goto(urlhtml);
